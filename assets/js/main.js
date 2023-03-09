00654afcff0083f9b1a2127773bd41e081e5f4f0
Shadow79892 <el_shadow79892@hotmail.com>
2023-03-09 15:49:19 -0400

    Terminado

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+// let areaCv = document.getElementById("area-cv");
+
+
+
+// function generateResume(){
+//     html2pdf(areaCv)
+// }
+
+// let resumeButton = document.getElementById("resumeButton");
+
+
+// resumeButton.addEventListener("click", ()=>{
+
+//     areaCv.classList.add("scale-cv");
+
+
+//     generateResume();
+// })
